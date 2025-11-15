f3652f93c551784e84ea520c1952962ad521ce1d
MawiraVictor <Victor.Mawira@strathmore.edu>
2025-11-15 12:35:36 +0300

    learning-functions in js

--- tutorial2.js
+++ tutorial2.js
@@ -1,21 +1,21 @@
 // creating a text baset calculator
 
 const prompt = require("prompt-sync")()
-// function getNumber(numberString) {
-//     /*
-//     getNumber() => the () are called parameters
-// => A parameter is a special kind of variable used in a funtion to refer to one 
-//    of the pieces of data provided as input to the funtion 
-//     */  
-//    while (true){
-//     const  number = parseFloat(prompt('Enter Number ' + numberString + ": "))
-//     if (isNaN(number)){
-//         console.log('Invalid Input')
-//     }else {
-//         return number
-//     }
-// }
-// }
+function getNumber(numberString) {// this funtion helps us to reuse out code. instead of having multiple while loops
+    /*
+    getNumber() => the () are called parameters
+  => A parameter is a special kind of variable used in a funtion to refer to one 
+   of the pieces of data provided as input to the funtion 
+    */  
+   while (true){
+    const  number = parseFloat(prompt('Enter Number ' + numberString + ": "))
+    if (isNaN(number)){
+        console.log('Invalid Input')
+    }else {
+        return number
+    }
+}
+}
 const number1 = getNumber('One');
 const number2 = getNumber('Two');
 
