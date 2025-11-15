e1f4cced74a49b55aebf09d08bc830395346143f
MawiraVictor <Victor.Mawira@strathmore.edu>
2025-11-15 12:20:14 +0300

    simple two Num calc with a loop funtion

---
+++ tutorial2.js
@@ -0,0 +1,74 @@
+// creating a text baset calculator
+
+const prompt = require("prompt-sync")()
+function getNumber(numberString) {
+    /*
+    getNumber() => the () are called parameters
+=> A parameter is a special kind of variable used in a funtion to refer to one 
+   of the pieces of data provided as input to the funtion 
+    */  
+   while (true){
+    const  number = parseFloat(prompt('Enter Number ' + numberString + ": "))
+    if (isNaN(number)){
+        console.log('Invalid Input')
+    }else {
+        return number
+    }
+}
+}
+const number1 = getNumber('One');
+const number2 = getNumber('Two');
+
+
+const operator = prompt('Enter sign: \nchoose the following signs +, -, *, /: ')
+// check the value entered 
+/*
+   Arithmetic operators
+    * => multiply
+    / => division
+    + => addition
+    - => subtraction
+ */
+
+// Understanding the Switch statement
+let result;
+let valid = true;
+
+switch(operator){
+    case "+": //addition
+        result = number1 + number2
+        break;
+    case "*": //multiplacation of the two numbers input
+        result = number1 * number2
+        break; // break  helps to close the case
+    case "/":
+        result = number1 / number2 // this is division of the two numbers
+        break; 
+    case "-": // this is subtraction of the two numbers
+        result = number1 - number2
+        break
+    default:
+        console.log("Invalid input")
+        valid = false
+        break
+}
+
+if (valid) console.log(number1, operator,  number2, "=", result)
+
+   
+
+// function getNumber(numberString) {
+//     /*
+//     getNumber() => the () are called parameters
+// => A parameter is a special kind of variable used in a funtion to refer to one 
+//    of the pieces of data provided as input to the funtion 
+//     */  
+//    while (true){
+//     const  number = parseFloat(prompt('Enter Number ' + numberString + ": "))
+//     if (isNaN(number2)){
+//         console.log('Invalid Input')
+//     }else {
+//         return number
+//     }
+// }
+// }
